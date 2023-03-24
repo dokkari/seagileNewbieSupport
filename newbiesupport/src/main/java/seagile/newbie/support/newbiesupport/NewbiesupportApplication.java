8b8f5ca5af64ee131a9645d8c3d6502f2ed00523
Juhee Kim <kjuhee_kr@naver.com>
2023-03-24 14:42:09 +0900

    initilaize

---
+++ newbiesupport/src/main/java/seagile/newbie/support/newbiesupport/NewbiesupportApplication.java
@@ -0,0 +1,13 @@
+package seagile.newbie.support.newbiesupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbiesupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewbiesupportApplication.class, args);
+	}
+
+}
---
+++ newbiesupport/src/test/java/seagile/newbie/support/newbiesupport/NewbiesupportApplicationTests.java
@@ -0,0 +1,13 @@
+package seagile.newbie.support.newbiesupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbiesupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
